ae8b069e70c6dc642d5ce388b737e0998ea27e86
ltt102858 <821449952@qq.com>
2020-06-29 09:27:59 +0800

    添加项目文件。

---
+++ MyFirstTest/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MyFirstTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHostBuilder hostBuilder = CreateHostBuilder(args);
+            IHost builder = hostBuilder.Build();
+            builder.Run();
+            //CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
